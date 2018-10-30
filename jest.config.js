9f88227106c7258b98851af193c26a0d170cceae
linuxing3 <linuxing3@qq.com>
2018-10-30 18:36:01 -0400

    add coverage

--- jest.config.js
+++ jest.config.js
@@ -11,4 +11,7 @@ module.exports = {
   snapshotSerializers: ["jest-serializer-vue"],
   testMatch: ["**/tests/unit/**/*.spec.(js|jsx|ts|tsx)|**/__tests__/*.(js|jsx|ts|tsx)"],
   testURL: "http://localhost/",
+  collectCoverage: true,
+  collectCoverageFrom: ["src/**/*.{js,ts,vue}", "!**/node_modules/**"],
+  coverageReporters: ["html", "text-summary"],
 };
