3d35c639f142762aac193e56d7b46f32dbf9be00
linuxing3 <linuxing3@qq.com>
2018-10-31 19:51:46 -0400

    apply prisma interface to activity

--- src/api/models/Activity.ts
+++ src/api/models/Activity.ts
@@ -1,13 +1,17 @@
-interface IActivity {
-  id: number;
-  name: string;
-  description: string;
-}
+import { ActivityNode } from "@/api/prisma";
 
-const Activity: IActivity = {
-  id: 1,
-  name: "activity title",
-  description: "activity description ",
+const Activity: ActivityNode = {
+  id: "1",
+  occurrenceDate: "2018-10-30",
+  content: "XXX",
+  currentDate: "2018-10-30",
+  startTime: "2018-10-30",
+  endTime: "2018-10-30",
+  reportDate: "2018-10-30",
+  reportContent: "XXX",
+  instructionDate: "2018-10-30",
+  instruction: "XXX",
+  priority: "HIGH",
 };
 
 export default Activity;
