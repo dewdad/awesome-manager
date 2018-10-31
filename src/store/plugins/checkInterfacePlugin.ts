a54e2a86b0156e5d517a05ff3cd24b1595ed6396
linuxing3 <linuxing3@qq.com>
2018-10-31 10:28:44 -0400

    fixed checkInterface

--- src/store/plugins/checkInterfacePlugin.ts
+++ src/store/plugins/checkInterfacePlugin.ts
@@ -1,33 +1,33 @@
 interface IPayload {
-    [key: string] : any
+  [key: string]: any;
 }
 
-type Omit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>
+type Omit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>;
 
-type PayloadProps = Pick<IPayload, "id"|"name">
-type PayloadOmmitedProps = Omit<IPayload, "id"|"name">
+type PayloadProps = Pick<IPayload, "id" | "name">;
+type PayloadOmmitedProps = Omit<IPayload, "id" | "name">;
 
 export const CheckInterfacePlugin = (options: any) => {
   return (store: any) => {
     let namespace = options.namespace || "";
     // use proper interface
-    if(namespace === "activity") {
-        // use ActiviyInterface
+    if (namespace === "activity") {
+      // use ActiviyInterface
     }
     // Every Mutations will be logged
     store.subscribe((mutation: any) => {
-        if(mutation.type === `{namespace}/actionCreate`) {
-            return type of mutation.payload === PayloadInterface? true: false
-        }
-        if(mutation.type === `{namespace}/actionDelete`) {
-            return type of mutation.payload === PayloadInterface? true: false
-        }
-        if(mutation.type === `{namespace}/actionRead`) {
-            return type of mutation.payload === PayloadInterface? true: false
-        }
-        if(mutation.type === `{namespace}/actionUpdate`) {
-            return type of mutation.payload === PayloadInterface? true: false
-        }
+      if (mutation.type === `{namespace}/actionCreate`) {
+        // return typeof mutation.payload === IPayload ? true : false;
+      }
+      if (mutation.type === `{namespace}/actionDelete`) {
+        // return typeof mutation.payload === IPayload ? true : false;
+      }
+      if (mutation.type === `{namespace}/actionRead`) {
+        // return typeof mutation.payload === IPayload ? true : false;
+      }
+      if (mutation.type === `{namespace}/actionUpdate`) {
+        // return typeof mutation.payload === IPayload ? true : false;
+      }
     });
   };
 };
