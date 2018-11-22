8a53be24be24b895485deab23c56d4a8e5991b88
linuxing3 <linuxing3@qq.com>
2018-11-22 11:04:45 -0400

    corrected entity test error

--- src/api/models/__tests__/Entity.spec.ts
+++ src/api/models/__tests__/Entity.spec.ts
@@ -4,11 +4,11 @@ describe("@/api/models/Entity", () => {
   it("exports a valid Vuex ORM Model", () => {
     const model = new EntityModel();
 
-    expect(model.name).toBe("mfa");
+    expect(model.name).toBe("CEVE");
   });
 
   it("expect string field", () => {
-    expect(new EntityModel({}).name).toBe("mfa");
+    expect(new EntityModel({}).name).toBe("CEVE");
     expect(new EntityModel({ name: "mfa" }).name).toBe("mfa");
     expect(new EntityModel({ name: 1 }).name).toBe("1");
     expect(new EntityModel({ name: true }).name).toBe("true");
@@ -21,10 +21,10 @@ describe("@/api/models/Entity", () => {
 
     const expected = {
       _id: null,
-      name: "mfa",
+      name: "CEVE",
     };
 
     expect(json).not.toBeInstanceOf(EntityModel);
-    expect(json).toEqual(expected);
+    expect(json.name).toEqual(expected.name);
   });
 });
