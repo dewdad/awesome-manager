cbb8155546e4c2e775c053f48b9c74ffda60e3e5
linuxing3 <linuxing3@qq.com>
2018-11-22 23:33:25 -0400

    Quick commit to master

--- src/api/mongodb/index.ts
+++ src/api/mongodb/index.ts
@@ -1,64 +1,64 @@
-/**
- * All the user information will be documents in MongoDB
- * This class sets up the connection depending on the environment
- */
-//use mongoose as the ORM
-const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
+// /**
+//  * All the user information will be documents in MongoDB
+//  * This class sets up the connection depending on the environment
+//  */
+// //use mongoose as the ORM
+// const mongoose = require("mongoose");
+// const Schema = mongoose.Schema;
 
-export class Mongo {
-  config;
-  connection_string;
-  mongodb;
-  constructor(config) {
-    this.config = config || {
-      mongodb: {
-        ip: "127.0.0.1",
-        port: "27017",
-        app: "test",
-      },
-    };
-    this.connection_string =
-      this.config.mongodb.ip + ":" + config.mongodb.port + "/" + config.mongodb.app;
-    this.init();
-  }
+// export class Mongo {
+//   config;
+//   connection_string;
+//   mongodb;
+//   constructor(config) {
+//     this.config = config || {
+//       mongodb: {
+//         ip: "127.0.0.1",
+//         port: "27017",
+//         app: "test",
+//       },
+//     };
+//     this.connection_string =
+//       this.config.mongodb.ip + ":" + config.mongodb.port + "/" + config.mongodb.app;
+//     this.init();
+//   }
 
-  /**
-   * Init a database connection and return the connection
-   * @returns {Object} mongodb - mongoose connection
-   * @throws {Error} err - error handling
-   */
-  init() {
-    try {
-      mongoose.connect(this.connection_string);
-      this.mongodb = mongoose.connection;
-    } catch (err) {
-      throw new Error(err);
-    }
-  }
+//   /**
+//    * Init a database connection and return the connection
+//    * @returns {Object} mongodb - mongoose connection
+//    * @throws {Error} err - error handling
+//    */
+//   init() {
+//     try {
+//       mongoose.connect(this.connection_string);
+//       this.mongodb = mongoose.connection;
+//     } catch (err) {
+//       throw new Error(err);
+//     }
+//   }
 
-  /**
-   * Create and return a mongoose model
-   * @param {String} modelName - Name of model
-   * @param {Object} schemaOptions - Shape of schema
-   * @example
-   * createModel("User", { name: String })
-   */
-  createModel(modelName, schemaOptions) {
-    return mongoose.model(modelName, new Schema(schemaOptions || { name: String }));
-  }
-}
+//   /**
+//    * Create and return a mongoose model
+//    * @param {String} modelName - Name of model
+//    * @param {Object} schemaOptions - Shape of schema
+//    * @example
+//    * createModel("User", { name: String })
+//    */
+//   createModel(modelName, schemaOptions) {
+//     return mongoose.model(modelName, new Schema(schemaOptions || { name: String }));
+//   }
+// }
 
-const mongo = new Mongo({
-  mongodb: {
-    ip: "192.168.0.100",
-    port: "27017",
-    app: "test",
-  },
-});
+// const mongo = new Mongo({
+//   mongodb: {
+//     ip: "192.168.0.100",
+//     port: "27017",
+//     app: "test",
+//   },
+// });
 
-export default mongo;
+// export default mongo;
 
-// const Document = mongo.createDocument({});
-// const doc = new Document();
-// doc.save();
+// // const Document = mongo.createDocument({});
+// // const doc = new Document();
+// // doc.save();
