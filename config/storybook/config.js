8b51131dc81b064a9dde8024577b9bf2ecdaa4a6
linuxing3 <linuxing3@qq.com>
2018-11-23 18:00:18 -0400

    fixed storybook without babel loader error

--- config/storybook/config.js
+++ config/storybook/config.js
@@ -1,10 +1,22 @@
 /* eslint-disable import/no-extraneous-dependencies */
-import { configure } from '@storybook/vue'
+import { configure } from "@storybook/vue";
+import Vue from "vue";
+import Vuex from "vuex"; // Vue plugins
+import VueRx from "vue-rx"; // Vue plugins
 
-const req = require.context('../../src/stories', true, /.stories.js$/)
+// Import your custom components.
+import MyButton from "../../src/components/base/MyButton.vue";
+
+const req = require.context("../../src/stories", true, /.stories.js$/);
+
+// Install Vue plugins.
+Vue.use(Vuex);
+Vue.use(VueRx);
+// Register custom components.
+Vue.component("my-button", MyButton);
 
 function loadStories() {
-  req.keys().forEach(filename => req(filename))
+  req.keys().forEach(filename => req(filename));
 }
 
-configure(loadStories, module)
+configure(loadStories, module);
--- src/stories/index.stories.js
+++ src/stories/index.stories.js
@@ -3,23 +3,33 @@ import { storiesOf } from "@storybook/vue";
 import { action } from "@storybook/addon-actions";
 import { linkTo } from "@storybook/addon-links";
 
-import MyButton from "../components/MyButton.vue";
+import MyButton from "../components/base/MyButton.vue";
+import RxCounter from "../components/widgets/RxCounter.vue";
 
-storiesOf("Button", module)
-  .add("with text", () => ({
-    components: { MyButton },
-    template: '<my-button @click="action">Hello Button</my-button>',
-    methods: { action: action("clicked") },
-  }))
-  .add("with JSX", () => ({
-    components: { MyButton },
-    render() {
-      return <my-button onClick={this.action}>With JSX</my-button>;
-    },
-    methods: { action: linkTo("Button", "with some emoji") },
-  }))
-  .add("with some emoji", () => ({
-    components: { MyButton },
-    template: '<my-button @click="action">😀 😎 👍 💯</my-button>',
-    methods: { action: action("clicked") },
-  }));
+storiesOf("Button", module).add("with text", () => ({
+  components: { MyButton },
+  template: '<my-button @click="action">Hello Button</my-button>',
+  methods: { action: action("clicked") },
+}));
+storiesOf("Button", module).add("with other text", () => ({
+  components: { MyButton },
+  template: '<my-button @click="action">RX Button</my-button>',
+  methods: { action: action("Rx clicked") },
+}));
+storiesOf("Counter", module).add("Rx Counter", () => ({
+  components: { RxCounter },
+  template: "<rx-counter></rx-counter>",
+  methods: { action: action("count$") },
+}));
+// .add("with JSX", () => ({
+//   components: { MyButton },
+//   render() {
+//     return <my-button onClick={this.action}>With JSX</my-button>;
+//   },
+//   methods: { action: linkTo("Button", "with some emoji") },
+// }))
+// .add("with some emoji", () => ({
+//   components: { MyButton },
+//   template: '<my-button @click="action">😀 😎 👍 💯</my-button>',
+//   methods: { action: action("clicked") },
+// }));
