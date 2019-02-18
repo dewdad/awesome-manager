7bd3caa0df4f703b810e32e335eeac3c8663e4f1
linuxing3 <linuxing3@qq.com>
2019-02-18 19:50:37 -0400

    simplify vue.config

--- vue.config.js
+++ vue.config.js
@@ -1,65 +1,3 @@
-/* eslint-disable */
-const path = require('path');
-
-/* add vue hotloader */
-function addVueJSXHotLoader(config) {
-  config.module
-    .rule(/\.(j|t)sx$/)
-    .test(/\.(j|t)sx$/)
-    .use('vue-jsx-hot-loader')
-    .before('babel-loader')
-    .loader('vue-jsx-hot-loader');
-}
-
-/* add copy plugin */
-function addCopyPlugin(config) {
-  /* config.plugin('copy') */
-  config.plugin('copy').tap(args => {
-    return [
-      ...args,
-      {
-        from: path.resolve('./public/template'),
-        to: path.resolve('./dist/template'),
-        toType: 'dir',
-      },
-    ];
-  });
-}
-
-/* add define plugin */
-function addDefinePlugin(config) {
-  /* config.plugin('define') */
-  config.plugin('define').tap(args => {
-    args[0]['process.env']['STATIC'] = '"/"';
-    return args;
-  });
-}
-
-/* add module/loader */
-function addModule(config) {
-  /* config.module.rule('vue') */
-  config.module
-    .rule('vue')
-    .use('vue-loader') // use数组
-    .loader('vue-loader') // 选取loader并tap选项
-    .tap(options => {
-      // 修改它的选项...
-      options.compilerOptions = {
-        preserveWhitespace: false,
-      };
-      return options; // 必须返回
-    });
-}
-
-/* add html plugin */
-function addHTMLPlugin(config) {
-  /* config.plugin('html') */
-  config.plugin('html').tap(args => {
-    args[0].template = path.resolve('public/index.html');
-    return args;
-  });
-}
-
 /* add entry file */
 function addEntry(config) {
   // 修改程序入口文件
@@ -90,31 +28,13 @@ const storybook = {
 
 module.exports = {
   configureWebpack: {
-    devtool: 'source-map',
-    resolve: {
-      alias: require('./aliases.config').webpack,
-    },
+    devtool: 'source-map'
   },
   chainWebpack: config => {
     addEntry(config);
-    addModule(config);
-    addDefinePlugin(config);
-    addHTMLPlugin(config);
-    addCopyPlugin(config);
-    addVueJSXHotLoader(config);
   },
   pluginOptions: {
     i18n: i18n,
-    storybook: storybook,
-    electronBuilder: {
-      chainWebpackRendererProcess: config => {
-        config.plugin('define').tap(args => {
-          args[0]['IS_ELECTRON'] = true;
-          return args;
-        });
-      },
-      // outputDir: '',
-      disableMainProcessTypescript: false,
-    },
+    storybook: storybook
   },
 };
